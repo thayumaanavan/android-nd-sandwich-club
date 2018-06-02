eafc02d6901afb7476c0e77cd24c8c171fa060c3
Thayumaanavan Chitra Rajan <thayumaanavan.chitra-rajan@dxc.com>
2018-06-02 16:50:29 +0530

    Add json parsing,update details page and connect both of them.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,9 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,12 +17,21 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    private TextView placeOfOriginTv;
+    private TextView alsoKnownAsTv;
+    private TextView ingredientsTv;
+    private TextView descriptionTv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        placeOfOriginTv = findViewById(R.id.origin_tv);
+        alsoKnownAsTv = findViewById(R.id.also_known_tv);
+        ingredientsTv = findViewById(R.id.ingredients_tv);
+        descriptionTv = findViewById(R.id.description_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +54,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +67,13 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        placeOfOriginTv.setText(sandwich.getPlaceOfOrigin());
+        if(!sandwich.getAlsoKnownAs().isEmpty())
+            alsoKnownAsTv.setText(TextUtils.join(",",sandwich.getAlsoKnownAs()));
+        if(!sandwich.getIngredients().isEmpty())
+            ingredientsTv.setText(TextUtils.join(",",sandwich.getIngredients()));
+        descriptionTv.setText(sandwich.getDescription());
 
     }
 }
